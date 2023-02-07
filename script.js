0e276a2108d2a90785f3727c333d0ef45ec4edc7
Salam El-Banna <salambanna164@gmail.com>
2023-02-07 13:55:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const counter = document.getElementById("counter");
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+const reset = document.getElementById("reset");
+
+let count = 0;
+
+increase.addEventListener("click", function() {
+  count++;
+  counter.innerHTML = count;
+});
+
+decrease.addEventListener("click", function() {
+  count--;
+  counter.innerHTML = count;
+});
+
+reset.addEventListener("click", function() {
+  count = 0;
+  counter.innerHTML = count;
+});
