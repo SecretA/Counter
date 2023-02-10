e62b0aa8d00b962a8fc361989451502c45822676
Salam El-Banna <salambanna164@gmail.com>
2023-02-10 14:26:50 +0200

    update version and cache files

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,10 @@
-const cacheName = 'counter-pwa-v1';
+const cacheName = 'digital-counter-v:1.4';
 const filesToCache = [
   '/Counter/',
   '/Counter/index.html',
   '/Counter/style.css',
-  '/Counter/app.js'
+  '/Counter/script.js',
+  '/Counter/img/*'
 ];
 
 self.addEventListener('install', event => {
@@ -28,3 +29,52 @@ self.addEventListener('fetch', event => {
       })
   );
 });
+
+
+
+
+// // This is the "Offline page" service worker
+
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// const CACHE = "pwabuilder-page";
+
+// const offlineFallbackPage = "index.html";
+
+// self.addEventListener("message", (event) => {
+//   if (event.data && event.data.type === "SKIP_WAITING") {
+//     self.skipWaiting();
+//   }
+// });
+
+// self.addEventListener('install', async (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE)
+//       .then((cache) => cache.add(offlineFallbackPage))
+//   );
+// });
+
+// if (workbox.navigationPreload.isSupported()) {
+//   workbox.navigationPreload.enable();
+// }
+
+// self.addEventListener('fetch', (event) => {
+//   if (event.request.mode === 'navigate') {
+//     event.respondWith((async () => {
+//       try {
+//         const preloadResp = await event.preloadResponse;
+
+//         if (preloadResp) {
+//           return preloadResp;
+//         }
+
+//         const networkResp = await fetch(event.request);
+//         return networkResp;
+//       } catch (error) {
+//         const cache = await caches.open(CACHE);
+//         const cachedResp = await cache.match(offlineFallbackPage);
+//         return cachedResp;
+//       }
+//     })());
+//   }
+// });
