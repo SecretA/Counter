e1b897e0432ce5a417d96afca7e6b06f36fba35f
Salam El-Banna <salambanna164@gmail.com>
2023-02-11 03:21:16 +0200

    add offline support

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,8 @@ const filesToCache = [
   'script.js',
   'img/background.jpg',
   'img/counter_layout.png',
-  'img/icon.png'
+  'img/icon.png',
+  'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js'
 ];
 
 self.addEventListener('install', event => {
@@ -29,4 +30,51 @@ self.addEventListener('fetch', event => {
         return fetch(event.request);
       })
   );
+});
+
+
+
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "dc-offline-cache";
+const offlineFallbackPage = "index.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
